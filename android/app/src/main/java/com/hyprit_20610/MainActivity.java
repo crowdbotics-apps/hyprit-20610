49bc2f7dc02b7d56ba7e4dcc43917b158d80a542
Crowdbotics <team@crowdbotics.com>
2020-09-23 18:34:39 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/hyprit_20610/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hyprit_20610;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "hyprit_20610";
+    }
+}
